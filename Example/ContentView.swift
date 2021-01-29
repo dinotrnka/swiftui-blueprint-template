36e9df4fe755da082adfa578e28861e2cd77e746
dinotrnka <dinotrnka@gmail.com>
2021-01-29 13:13:53 +0100

    Initial project push

---
+++ Example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Example
+//
+//  Created by Dino Trnka on 29. 1. 2021..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Example/ExampleApp.swift
@@ -0,0 +1,18 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Dino Trnka on 29. 1. 2021..
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
+
