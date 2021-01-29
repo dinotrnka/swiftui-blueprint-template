0561901377ba39f33ea8ad918eb5ae774563be90
dinotrnka <dinotrnka@gmail.com>
2021-01-29 17:07:48 +0100

    Folder restructuring

---
+++ Example/Code/Features/Landing/Views/LandingView.swift
@@ -0,0 +1,20 @@
+//
+//  LandingView.swift
+//  Example
+//
+//  Created by Dino Trnka on 29. 1. 2021..
+//
+
+import SwiftUI
+
+struct LandingView: View {
+  var body: some View {
+    Text("Hello there friend!")
+  }
+}
+
+struct LandingView_Previews: PreviewProvider {
+  static var previews: some View {
+    LandingView()
+  }
+}
---
+++ Example/Code/Features/Root/Views/RootView.swift
@@ -0,0 +1,20 @@
+//
+//  RootView.swift
+//  Example
+//
+//  Created by Dino Trnka on 29. 1. 2021..
+//
+
+import SwiftUI
+
+struct RootView: View {
+  var body: some View {
+    LandingView()
+  }
+}
+
+struct RootView_Previews: PreviewProvider {
+  static var previews: some View {
+    RootView()
+  }
+}
